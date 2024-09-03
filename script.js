9e8a356c435ecfbb5495b7f295cbbc13d335cd4d
dbreskovit <diegobresko@gmail.com>
2024-09-03 11:23:41 -0300

    Update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var graph = {
   nodes: [
-
     { id: "Roteador", group: 1, repeater: false, coordinates: "0,0", speed: "100mbps", dbm: "-50", isRouter: true },
 
     { id: "Repetidor 1", group: 2, repeater: true, coordinates: "100,100", speed: "50mbps", dbm: "-60" },
@@ -9,45 +8,42 @@ var graph = {
     { id: "Repetidor 4", group: 5, repeater: true, coordinates: "-100,-100", speed: "50mbps", dbm: "-60" },
     
     // Roteador 1
-    { id: "Secretaria", group: 1, repeater: false, coordinates: "2729357374,53235855863", speed: "50mbps", dbm: "-60" },
-    { id: "Sala Diretora", group: 1, repeater: false, coordinates: "272934634,53235860878", speed: "50mbps", dbm: "-60" },
-    { id: "Sala Vice Diretora", group: 1, repeater: false, coordinates: "2729343651,53235855269", speed: "50mbps", dbm: "-60" },
+    { id: "Secretaria", group: 1, repeater: false, coordinates: "27°29'3,57374\":53°23'58,55863", speed: "10.0mbps", dbm: "-61" },
+    { id: "Sala Diretora", group: 1, repeater: false, coordinates: "27°29'3,4634\":53°23'58,60878", speed: "10.0mbps", dbm: "-73" },
+    { id: "Sala Vice Diretora", group: 1, repeater: false, coordinates: "27°29'3,43651\":53°23'58,55269", speed: "10.0mbps", dbm: "-73" },
     
-    
+    // Repetidor 1
+    { id: "Convivência 3", group: 2, repeater: false, coordinates: "27°29'3,83752\":53°23'59,08747", speed: "9.6mbps", dbm: "-61" },
+    { id: "Sala 7", group: 2, repeater: false, coordinates: "27°29'3,79925\":53°23'58,92097", speed: "9.6mbps", dbm: "-57" },
+    { id: "Sala dos Professores", group: 2, repeater: false, coordinates: "27°29'4,01093\":53°23'58,78223", speed: "9.2mbps", dbm: "-51" },
+    { id: "Sala SOE", group: 2, repeater: false, coordinates: "27°29'4,01215\":53°23'58,62221", speed: "9.5mbps", dbm: "-70" },
+    { id: "Sala da Coordenação", group: 2, repeater: false, coordinates: "27°29'4,14982\":53°23'58,3696", speed: "9.7mbps", dbm: "-51" },
+    { id: "Sala AEE", group: 2, repeater: false, coordinates: "27°29'4,29817\":53°23'58,4682", speed: "9.3mbps", dbm: "-61" },
+    { id: "Convivência 1", group: 2, repeater: false, coordinates: "27°29'2,95253\":53°23'57,34561", speed: "9.5mbps", dbm: "-42" },
+    { id: "Cozinha", group: 2, repeater: false, coordinates: "27°29'3,83489\":53°23'58,06849", speed: "7.9mbps", dbm: "-62" },
+    { id: "Ginásio", group: 2, repeater: false, coordinates: "27°48'4,431\":53°39'94,27", speed: "9.0mbps", dbm: "-55" },
 
-  // Repetidor 1
-    { id: "Convivência 3", group: 2, repeater: false, coordinates: "2729383752,53235908747", speed: "50mbps", dbm: "-60" },
-    { id: "Sala 7", group: 2, repeater: false, coordinates: "2729379925,53235892097", speed: "50mbps", dbm: "-60" },
-    { id: "Sala dos Professores", group: 2, repeater: false, coordinates: "2729401093,53235878223", speed: "50mbps", dbm: "-60" },
-    { id: "Sala SOE", group: 2, repeater: false, coordinates: "2729401215,53235862221", speed: "50mbps", dbm: "-60" },
-    { id: "Sala da Cordenação", group: 2, repeater: false, coordinates: "2729414982,5323583696", speed: "50mbps", dbm: "-60" },
-    { id: "Sala AEE", group: 2, repeater: false, coordinates: "2729429817,5323584682", speed: "50mbps", dbm: "-60" },
-    { id: "Convivência 1", group: 2, repeater: false, coordinates: "2729295253,53235734561", speed: "50mbps", dbm: "-60" },
-    { id: "Cozinha", group: 2, repeater: false, coordinates: "2729383489,53235806849", speed: "50mbps", dbm: "-60" },
-    { id: "Ginásio", group: 2, repeater: false, coordinates: "27484431,53399427", speed: "50mbps", dbm: "-60" },
-
-  // Repetidor 2
-    { id: "Laboratório de Informática", group: 3, repeater: false, coordinates: "2729447947,53235913816", speed: "50mbps", dbm: "-60" },
-    { id: "Sala 9", group: 3, repeater: false, coordinates: "2729455737,5323588845", speed: "50mbps", dbm: "-60" },
-    { id: "Sala 8", group: 3, repeater: false, coordinates: "2729441438,53235876232", speed: "50mbps", dbm: "-60" },
-    { id: "Convivência 4", group: 3, repeater: false, coordinates: "2729416429,53235880991", speed: "50mbps", dbm: "-60" },
-    { id: "Sala 10", group: 3, repeater: false, coordinates: "2729430782,53235921923", speed: "50mbps", dbm: "-60" },
-    { id: "Laboratório", group: 3, repeater: false, coordinates: "2729412508,53235925638", speed: "50mbps", dbm: "-60" },
-    { id: "Biblioteca", group: 3, repeater: false, coordinates: "2729416137,53235907145", speed: "50mbps", dbm: "-60" },
+    // Repetidor 2
+    { id: "Laboratório de Informática", group: 3, repeater: false, coordinates: "27°29'4,47947\":53°23'59,13816", speed: "9.0mbps", dbm: "-32" },
+    { id: "Sala 9", group: 3, repeater: false, coordinates: "27°29'4,55737\":53°23'58,8845", speed: "9.3mbps", dbm: "-32" },
+    { id: "Sala 8", group: 3, repeater: false, coordinates: "27°29'4,41438\":53°23'58,76232", speed: "9.4mbps", dbm: "-68" },
+    { id: "Convivência 4", group: 3, repeater: false, coordinates: "27°29'4,16429\":53°23'58,80991", speed: "9.6mbps", dbm: "-61" },
+    { id: "Sala 10", group: 3, repeater: false, coordinates: "27°29'4,30782\":53°23'59,21923", speed: "9.3mbps", dbm: "-38" },
+    { id: "Laboratório", group: 3, repeater: false, coordinates: "27°29'4,12508\":53°23'59,25638", speed: "9.4mbps", dbm: "-55" },
+    { id: "Biblioteca", group: 3, repeater: false, coordinates: "27°29'4,16137\":53°23'59,07145", speed: "9.5mbps", dbm: "-51" },
     
-  // Repetidor 3
-    { id: "Sala 1", group: 4, repeater: false, coordinates: "2729334244,53235822923", speed: "50mbps", dbm: "-60" },
-    { id: "Sala 2", group: 4, repeater: false, coordinates: "2729317756,53235802547", speed: "50mbps", dbm: "-60" },
-    { id: "Sala 3", group: 4, repeater: false, coordinates: "2729305956,53235780123", speed: "50mbps", dbm: "-60" },
-    { id: "Sala 4", group: 4, repeater: false, coordinates: "272929854,53235767634", speed: "50mbps", dbm: "-60" },
-    { id: "Entrada", group: 4, repeater: false, coordinates: "27484232212164496,5339968124167569", speed: "50mbps", dbm: "-60" },
+    // Repetidor 3
+    { id: "Sala 1", group: 4, repeater: false, coordinates: "27°29'3,34244\":53°23'58,22923", speed: "9.4mbps", dbm: "-42" },
+    { id: "Sala 2", group: 4, repeater: false, coordinates: "27°29'3,17756\":53°23'58,02547", speed: "9.5mbps", dbm: "-45" },
+    { id: "Sala 3", group: 4, repeater: false, coordinates: "27°29'3,05956\":53°23'57,80123", speed: "9.4mbps", dbm: "-29" },
+    { id: "Sala 4", group: 4, repeater: false, coordinates: "27°29'2,9854\":53°23'57,67634", speed: "8.9mbps", dbm: "-46" },
+    { id: "Entrada", group: 4, repeater: false, coordinates: "27°48'4,232212164496\":53°39'96,8124167569", speed: "9.3mbps", dbm: "-60" },
   
-  // Repetidor 4
-    { id: "Convivência 2", group: 5, repeater: false, coordinates: "2729329687,53235783622", speed: "50mbps", dbm: "-60" },
-    { id: "Sala 5", group: 5, repeater: false, coordinates: "2729316363,53235713177", speed: "50mbps", dbm: "-60" },
-    { id: "Sala 6", group: 5, repeater: false, coordinates: "2729333733,53235732812", speed: "50mbps", dbm: "-60" },
-    { id: "Salão de Eventos", group: 5, repeater: false, coordinates: "2729355693,5323576811", speed: "50mbps", dbm: "-60" },
-
+    // Repetidor 4
+    { id: "Convivência 2", group: 5, repeater: false, coordinates: "27°29'3,29687\":53°23'57,83622", speed: "9.5mbps", dbm: "-42" },
+    { id: "Sala 5", group: 5, repeater: false, coordinates: "27°29'3,16363\":53°23'57,13177", speed: "9.3mbps", dbm: "-47" },
+    { id: "Sala 6", group: 5, repeater: false, coordinates: "27°29'3,33733\":53°23'57,32812", speed: "9.5mbps", dbm: "-47" },
+    { id: "Salão de Eventos", group: 5, repeater: false, coordinates: "27°29'3,55693\":53°23'57,6811", speed: "9.2mbps", dbm: "-75" },
   ],
   links: [
     { source: "Roteador", target: "Repetidor 1", value: 1 },
@@ -59,7 +55,7 @@ var graph = {
     { source: "Repetidor 1", target: "Sala 7", value: 1 },
     { source: "Repetidor 1", target: "Sala dos Professores", value: 1 },
     { source: "Repetidor 1", target: "Sala SOE", value: 1 },
-    { source: "Repetidor 1", target: "Sala da Cordenação", value: 1 },
+    { source: "Repetidor 1", target: "Sala da Coordenação", value: 1 },
     { source: "Repetidor 1", target: "Sala AEE", value: 1 },
     { source: "Repetidor 1", target: "Convivência 1", value: 1 },
     { source: "Repetidor 1", target: "Ginásio", value: 1 },
@@ -88,25 +84,24 @@ var graph = {
     { source: "Sala Diretora", target: "Roteador", value: 1 },
     { source: "Sala Vice Diretora", target: "Roteador", value: 1 },
 
-    { source: "Repetidor 1", target: "Repetidor 2", value: 1 },
-    { source: "Repetidor 1", target: "Repetidor 3", value: 1 },
-    { source: "Repetidor 1", target: "Repetidor 4", value: 1 },
+    // { source: "Repetidor 1", target: "Repetidor 2", value: 1 },
+    // { source: "Repetidor 1", target: "Repetidor 3", value: 1 },
+    // { source: "Repetidor 1", target: "Repetidor 4", value: 1 },
 
     { source: "Repetidor 2", target: "Repetidor 1", value: 1 },
     { source: "Repetidor 2", target: "Repetidor 3", value: 1 },
     { source: "Repetidor 2", target: "Repetidor 4", value: 1 },
 
-    // { source: "Repetidor 3", target: "Repetidor 1", value: 1 },
     { source: "Repetidor 3", target: "Repetidor 2", value: 1 },
     { source: "Repetidor 3", target: "Repetidor 4", value: 1 },
+    { source: "Repetidor 3", target: "Repetidor 1", value: 1 },
 
-    { source: "Repetidor 4", target: "Repetidor 1", value: 1 },
-    // { source: "Repetidor 4", target: "Repetidor 2", value: 1 },
+    { source: "Repetidor 4", target: "Repetidor 2", value: 1 },
     { source: "Repetidor 4", target: "Repetidor 3", value: 1 },
+    { source: "Repetidor 4", target: "Repetidor 1", value: 1 },
 
   ],
 };
-
 
 var svg = d3.select("svg"),
   width = window.innerWidth,
@@ -314,29 +309,29 @@ function ticked() {
     .attr("cy", function (d) {
       return d.y;
     })
-    .on("mouseover", function (event, d) {
-      tooltip.transition().duration(200).style("opacity", 0.9);
+    // .on("mouseover", function (event, d) {
+    //   tooltip.transition().duration(200).style("opacity", 0.9);
 
-      // Seleciona os elementos <coordinates>, <dbm> e <speed> dentro do nó e obtém seus textos
-      var coordinates = d3.select(this).select("coordinates").text();
-      var dbm = d3.select(this).select("dbm").text();
-      var speed = d3.select(this).select("speed").text();
+    //   // Seleciona os elementos <coordinates>, <dbm> e <speed> dentro do nó e obtém seus textos
+    //   var coordinates = d3.select(this).select("coordinates").text();
+    //   var dbm = d3.select(this).select("dbm").text();
+    //   var speed = d3.select(this).select("speed").text();
 
-      tooltip
-        .html(
-          "Coordenada: " +
-            coordinates +
-            "<br/>dBm: " +
-            dbm +
-            "<br/>Velocidade: " +
-            speed
-        )
-        .style("left", event.pageX + 5 + "px")
-        .style("top", event.pageY - 28 + "px");
-    })
-    .on("mouseout", function (d) {
-      tooltip.transition().duration(500).style("opacity", 0);
-    });
+    //   tooltip
+    //     .html(
+    //       "Coordenada: " +
+    //         coordinates +
+    //         "<br/>Frequência: " +
+    //         dbm +
+    //         "<br/>Velocidade: " +
+    //         speed
+    //     )
+    //     .style("left", event.pageX + 5 + "px")
+    //     .style("top", event.pageY - 28 + "px");
+    // })
+    // .on("mouseout", function (d) {
+    //   tooltip.transition().duration(500).style("opacity", 0);
+    // });
 
   updateGroups();
 }
@@ -425,19 +420,19 @@ function group_dragended(groupId) {
   d3.select(this).select("path").style("stroke-width", 1);
 }
 
-node.on("mouseover", function (event, d) {
-  tooltip.transition().duration(200).style("opacity", 0.9);
-  let coordinates = d.coordinates || "N/A";
-  let speed = d.speed || "N/A";
-  let dbm = d.dbm || "N/A";
-  let optionsHtml = d.options ? `<br>Opções: ${d.options.join(", ")}` : "";
-  tooltip
-    .html(
-      `Coordenadas: ${coordinates}<br>Velocidade: ${speed}<br>dBm: ${dbm}${optionsHtml}`
-    )
-    .style("left", event.pageX + 5 + "px")
-    .style("top", event.pageY - 28 + "px");
-});
+// node.on("mouseover", function (event, d) {
+//   tooltip.transition().duration(200).style("opacity", 0.9);
+//   let coordinates = d.coordinates || "N/A";
+//   let speed = d.speed || "N/A";
+//   let dbm = d.dbm || "N/A";
+//   let optionsHtml = d.options ? `<br>Opções: ${d.options.join(", ")}` : "";
+//   tooltip
+//     .html(
+//       `Coordenadas: ${coordinates}<br>Velocidade: ${speed}<br>Frequência: ${dbm}${optionsHtml}`
+//     )
+//     .style("left", event.pageX + 5 + "px")
+//     .style("top", event.pageY - 28 + "px");
+// });
 
 function ticked() {
   link
@@ -468,10 +463,12 @@ function ticked() {
 
      
       var coordinates = d3.select(this).select("coordinates").text();
+      var title = d3.select(this).select("title").text();
       var dbm = d3.select(this).select("dbm").text();
       var speed = d3.select(this).select("speed").text();
 
-      tooltip.html("Coordenada: " + coordinates + "<br/>dBm: " + dbm + "<br/>Velocidade: " + speed)
+      tooltip.html("Local: " + title + "<br/>" +
+        "Coordenada: " + coordinates + "<br/>Frequência: " + dbm + "dBm" + "<br/>Velocidade: " + speed)
         .style("left", (event.pageX + 5) + "px")
         .style("top", (event.pageY - 28) + "px");
     })
